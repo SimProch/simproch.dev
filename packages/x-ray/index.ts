50255233f30274d6ffd32ee371c2b4b2f75dd560
Simon Prochazka <prochazka.simon@hotmail.com>
2024-04-21 19:25:02 +0200

    chore: Moved x-ray to separate package

---
+++ packages/x-ray/index.ts
@@ -0,0 +1,3 @@
+export { default as XRaySvg } from './src/XRaySvg.svelte';
+export { default as XRayHtml } from './src/XRayHtml.svelte';
+export { default as XRayHtmlDarkMode } from './src/XRayHtmlDarkMode.svelte';
